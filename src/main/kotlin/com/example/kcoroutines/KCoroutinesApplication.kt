844d168b47cdca4f8fe20f75e532b54a4e869c07
Rejaine Lima <rlima@atlassian.com>
2020-11-03 10:51:45 +1100

    First coroutine basic example

--- src/main/kotlin/com/example/kcoroutines/KCoroutinesApplication.kt
+++ src/main/kotlin/com/example/kcoroutines/KCoroutinesApplication.kt
@@ -1,11 +1,19 @@
 package com.example.kcoroutines
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class KCoroutinesApplication
 
-fun main(args: Array<String>) {
-	runApplication<KCoroutinesApplication>(*args)
+fun main() {
+	GlobalScope.launch { // launch a new coroutine in background and continue
+		delay(1000L) // non-blocking delay for 1 second (1ms -> 1s)
+		print("World") // print after delay
+	}
+
+	print("Hello, ") // main thread continues while coroutine is delayed
+	Thread.sleep(2000L) // block main thread for 2s to keep JVM alive
 }
